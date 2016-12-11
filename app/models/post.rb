7446c82210fc70f561c96d295210367fa2657ff2
Dawna Ara <dawnaara@gmail.com>
2016-12-11 11:35:08 -0800

    worked on page styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
 
-  has_attached_file :image, styles: { :medium => "300x300#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { :medium => "400x400>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
